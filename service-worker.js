772a6911efdcfb53a95edd6d01969c976879a4bf
SoftWeekStudio <softweekstudio@gmail.com>
2026-09-05 21:34:35 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_VERSION="my-shopping-list-update-v1";
+const APP_SHELL=["./","./index.html","./manifest.webmanifest"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE_VERSION)
+      .then(cache=>cache.addAll(APP_SHELL))
+      .then(()=>self.skipWaiting())
+  );
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE_VERSION).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener("fetch",event=>{
+  const request=event.request;
+  if(request.mode==="navigate"){
+    event.respondWith(
+      fetch(request,{cache:"no-store"})
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE_VERSION).then(cache=>cache.put("./index.html",copy));
+          return response;
+        })
+        .catch(()=>caches.match("./index.html"))
+    );
+    return;
+  }
+  event.respondWith(caches.match(request).then(cached=>cached||fetch(request)));
+});
